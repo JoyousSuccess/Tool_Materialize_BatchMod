02e302c08cead61bff9cbf4619ff32dd1d112d11
JoyousSuccess <joyoussuccess777@gmail.com>
2021-03-16 15:44:30 -0500

    Initial commit

---
+++ Assets/Scripts/T_BatchFile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class T_BatchFile {
+  
+    public T_BatchFile (string inFileName, string inPath) {
+        fileName = inFileName;
+        path = inPath;
+    }
+    public T_BatchFile() { }
+
+#region [vars]
+    
+    public string fileName;
+    public string path;
+
+#endregion
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Materialize_BatchMod
+ Materialize ; Modded to batch process whole folder of albedo textures (hacky)
